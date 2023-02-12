3d075b79cfde6b04afa68366e9d0a5cb214913ac
Daria Yakkola <yakkolabest@gmail.com>
2023-02-12 18:30:44 +0300

    задача 8

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine()); 
+int firstNumbers = 2;
+while (firstNumbers <= N)
+{
+  Console.Write(firstNumbers + " ");
+  firstNumbers = firstNumbers + 2;
+}
